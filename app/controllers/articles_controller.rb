f8c38f455113dd20f83f48e068638f3f58b345df
Eric Suescun <edsuescun@gmail.com>
2019-03-08 19:03:35 -0500

    Se crea el modelo, se migra, se coordina con el controlador para que entren los datos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,23 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-	  render plain: params[:article].inspect
+	  @article = Article.new(article_params)
+	 
+		# if @article.save
+	 	#  		redirect_to @article, notice: "El artículo fue creado con éxito"
+		# else
+		# 	render :new
+		# end
+
+		@article.save
+  		redirect_to @article
 	end
+	 
+	private
+	  	def article_params
+	    	params.require(:article).permit(:title, :text)
+	  	end
 end
+
+
+
